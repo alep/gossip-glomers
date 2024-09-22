994234c50cda6f9f183b2a8ecd3b8f1b32303c9e
Alejandro Peralta <peralta.alejandro@gmail.come>
2024-09-22 19:48:48 -0300

    fixed counter and added kafka

--- src/counter.rs
+++ src/counter.rs
@@ -2,14 +2,11 @@ use async_trait::async_trait;
 use maelstrom::kv::{seq_kv, Storage, KV};
 use maelstrom::protocol::Message;
 use maelstrom::{done, Node, Result, Runtime};
+use rand;
 use serde::{Deserialize, Serialize};
-use std::cmp::max;
-use std::collections::HashMap;
 use std::sync::Arc;
-use std::sync::Mutex;
-use std::time::Duration;
+use std::usize;
 use tokio_context::context::Context;
-
 pub(crate) fn main() {
     let _ = Runtime::init(try_main());
 }
@@ -22,27 +19,12 @@ async fn try_main() -> Result<()> {
 
 #[derive(Clone)]
 struct Handler {
-    i: Arc<Mutex<Inner>>,
     s: Storage,
-}
-
-struct Inner {
-    c: HashMap<String, usize>,
 }
 
 impl Handler {
     fn new(runtime: Runtime) -> Self {
-        let initial: Vec<(String, usize)> = runtime
-            .nodes()
-            .iter()
-            .map(|x| -> (String, usize) { (x.to_string(), 0) })
-            .collect();
-        Self {
-            s: seq_kv(runtime),
-            i: Arc::new(Mutex::new(Inner {
-                c: HashMap::from_iter(initial),
-            })),
-        }
+        Self { s: seq_kv(runtime) }
     }
 }
 
@@ -51,41 +33,21 @@ impl Node for Handler {
     async fn process(&self, runtime: Runtime, req: Message) -> Result<()> {
         let msg: Result<Request> = req.body.as_obj();
         match msg {
-            Ok(Request::Init {}) => {
-                let (r0, h0) = (runtime.clone(), self.clone());
-
-                tokio::spawn(async move {
-                    loop {
-                        tokio::time::sleep(Duration::from_millis(5000)).await;
-
-                        let counter = {
-                            let i = h0.i.lock().unwrap();
-                            i.c.clone()
-                        };
-
-                        for to in ["n0", "n1", "n2"] {
-                            if to != r0.node_id() {
-                                let r1 = r0.clone();
-                                let counter0 = counter.clone();
-                                tokio::spawn(async move {
-                                    let (ctx, _handler) =
-                                        Context::with_timeout(Duration::from_millis(400));
-                                    let msg = Request::Replicate {
-                                        values: counter0.clone(),
-                                    };
-
-                                    let _message = r1.call(ctx, to, msg).await;
-                                });
-                            }
-                        }
-                    }
-                });
-            }
             Ok(Request::Read {}) => {
-                let value = {
-                    let i = self.i.lock().unwrap();
-                    i.c.values().sum()
-                };
+                let mut value: usize = 0;
+                for n in runtime.nodes() {
+                    let (ctx, _handler) = Context::new();
+                    let x = rand::random::<usize>();
+                    let s = format!("random_{:}_{:}", runtime.node_id(), x);
+                    let _ = self.s.put(ctx, s, x).await;
+                    let (ctx, _handler) = Context::new();
+                    let val = match self.s.get(ctx, n.to_string()).await {
+                        Ok(val) => val,
+                        Err(_) => 0,
+                    };
+                    value += val;
+                }
+
                 let read_response = Response::ReadOk { value };
                 return runtime.reply(req, read_response).await;
             }
@@ -100,23 +62,8 @@ impl Node for Handler {
                     .cas(ctx, runtime.node_id().to_string(), val, val + delta, true)
                     .await
                     .unwrap();
-                {
-                    let mut i = self.i.lock().unwrap();
-                    i.c.insert(runtime.node_id().to_string(), val + delta);
-                }
+
                 let response = Response::AddOk {};
-                return runtime.reply(req, response).await;
-            }
-            Ok(Request::Replicate { values }) => {
-                for n in ["n0", "n1", "n2"] {
-                    {
-                        let mut i = self.i.lock().unwrap();
-                        let v0 = i.c.get(n).copied().unwrap_or(0);
-                        let v1 = values.get(n).copied().unwrap_or(0);
-                        i.c.insert(n.to_string(), max(v0, v1));
-                    }
-                }
-                let response = Response::ReplicateOk {};
                 return runtime.reply(req, response).await;
             }
             _ => {}
@@ -131,7 +78,6 @@ enum Request {
     Init {},
     Read {},
     Add { delta: usize },
-    Replicate { values: HashMap<String, usize> },
 }
 
 #[derive(Serialize, Deserialize)]
@@ -139,5 +85,5 @@ enum Request {
 enum Response {
     ReadOk { value: usize },
     AddOk {},
-    ReplicateOk {},
 }
+
--- src/kafka.rs
+++ src/kafka.rs
@@ -1,101 +1,180 @@
 use async_trait::async_trait;
 
+use maelstrom::kv::{lin_kv, Storage, KV};
 use maelstrom::protocol::Message;
-use maelstrom::{done, Node, Result, Runtime};
-
+use maelstrom::{done, Error, Node, Result, Runtime};
 use serde::{Deserialize, Serialize};
 
-use std::collections::{HashMap, HashSet};
-use std::sync::Arc;
-use std::sync::Mutex;
-use std::time::Duration;
-use std::usize;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
-use tokio;
 use tokio_context::context::Context;
 
 pub(crate) fn main() {
-    let _ = Runtime::init(try_main());
+  let _ = Runtime::init(try_main());
 }
 
 async fn try_main() -> Result<()> {
-    let handler = Arc::new(Handler::new());
-    Runtime::new().with_handler(handler).run().await
+  let runtime = Runtime::new();
+  let handler = Arc::new(Handler::new(runtime.clone()));
+  runtime.with_handler(handler).run().await
 }
 
-#[derive(Clone, Default)]
+#[derive(Clone)]
 struct Handler {
-    inner: Arc<Mutex<State>>,
+  s: Storage,
+  inner: Arc<Mutex<State>>,
 }
 
 impl Handler {
-    fn new() -> Self {
-        Self {
-            inner: Arc::new(Mutex::new(State::default())),
-        }
+  fn new(runtime: Runtime) -> Self {
+    Self {
+      s: lin_kv(runtime),
+      inner: Arc::new(Mutex::new(State {
+        logs: HashMap::new(),
+      })),
     }
+  }
 }
 
 #[async_trait]
 impl Node for Handler {
-    async fn process(&self, runtime: Runtime, req: Message) -> Result<()> {
-        let msg: Result<Request> = req.body.as_obj();
-        match msg {
-            Ok(Request::Send { key, msg }) => {
-                {
-                    let mut state = self.inner.lock().unwrap();
-                    state.insert(key, msg)
-                }
-                let resp = Response::SendOk {};
-                return runtime.reply(req, resp).await;
-            }
-            Ok(Request::Poll {}) => {
-                let msgs = {
-                    let state = self.inner.lock().unwrap();
-                    state.poll()
-                };
-                let resp = Response::PollOk { msgs };
-                return runtime.reply(req, resp).await;
-            }
-            Ok(Request::Commit {}) => todo!(),
-            Ok(Request::LimitCommittedOffsets {}) => todo!(),
-            _ => {}
+  async fn process(&self, runtime: Runtime, req: Message) -> Result<()> {
+    let msg: Result<Request> = req.body.as_obj();
+    match msg {
+      Ok(Request::Send { key, msg }) => {
+        let mut offset: usize;
+        loop {
+          let offset_key = format!("{:}-offset", key);
+          let (ctx, _handler) = Context::new();
+          offset = self
+            .s
+            .get(ctx, offset_key.clone())
+            .await
+            .unwrap_or_else(|_| 0);
+
+          let (ctx, _handler) = Context::new();
+          let result: Result<()> = self
+            .s
+            .cas(ctx, offset_key.clone(), offset, offset + 1, true)
+            .await;
+
+          match result {
+            Ok(()) => break,
+            Err(e) => assert_eq!(
+              *e.downcast_ref::<Error>().unwrap(),
+              Error::PreconditionFailed
+            ),
+          };
         }
-        done(runtime, req)
+
+        let (ctx, _handler) = Context::new();
+        let _ = self.s.put(ctx, format!("{}-{}", key, offset), msg).await;
+
+        {
+          let mut inner = self.inner.lock().unwrap();
+          inner.insert(key, offset);
+        }
+
+        let resp = Response::SendOk { offset };
+        return runtime.reply(req, resp).await;
+      }
+      Ok(Request::Poll { offsets }) => {
+        let mut msgs = HashMap::<String, Vec<Log>>::new();
+
+        for (key, offset) in offsets {
+          let (ctx, _handler) = Context::new();
+
+          let last_offset = self
+            .s
+            .get(ctx, format!("{}-offset", key))
+            .await
+            .unwrap_or_else(|_| 0);
+
+          if last_offset == 0 {
+            continue;
+          }
+
+          let mut logs: Vec<Log> = vec![];
+          for idx in offset..last_offset {
+            let (ctx, _handler) = Context::new();
+            let v = self.s.get(ctx, format!("{}-{}", key, idx)).await.unwrap();
+            logs.push(Log(idx, v));
+          }
+          msgs.insert(key, logs);
+        }
+
+        let resp = Response::PollOk { msgs };
+        return runtime.reply(req, resp).await;
+      }
+      Ok(Request::CommitOffsets { offsets }) => {
+        for (key, offset) in offsets {
+          loop {
+            let commit_key = format!("{}-commit", key);
+            let (ctx, _handler) = Context::new();
+
+            let commit = self
+              .s
+              .get(ctx, commit_key.clone())
+              .await
+              .unwrap_or_else(|_| 0);
+
+            if commit >= offset {
+              break;
+            }
+
+            let (ctx, _handler) = Context::new();
+            let result: Result<()> = self
+              .s
+              .cas(ctx, commit_key.clone(), commit, offset, true)
+              .await;
+
+            match result {
+              Ok(()) => break,
+              Err(e) => assert_eq!(
+                *e.downcast_ref::<Error>().unwrap(),
+                Error::PreconditionFailed
+              ),
+            };
+          }
+        }
+
+        let resp = Response::CommitOffsetsOk {};
+        return runtime.reply(req, resp).await;
+      }
+      Ok(Request::ListCommittedOffsets { keys }) => {
+        let mut offsets = HashMap::<String, usize>::new();
+        for key in keys {
+          let commit_key = format!("{:}-commit", key);
+          let (ctx, _handler) = Context::new();
+
+          let commit = self
+            .s
+            .get(ctx, commit_key.clone())
+            .await
+            .unwrap_or_else(|_| 0);
+          if commit != 0 {
+            offsets.insert(key, commit);
+          }
+        }
+        let resp = Response::ListCommittedOffsetsOk { offsets };
+        return runtime.reply(req, resp).await;
+      }
+      _ => {}
     }
+    done(runtime, req)
+  }
 }
 
 #[derive(Default)]
 struct State {
-    offsets: HashMap<String, usize>,
-    committed: HashMap<String, usize>,
-    logs: HashMap<String, Vec<Log>>,
+  logs: HashMap<String, usize>,
 }
 
 impl State {
-    fn insert(&mut self, key: String, msg: usize) {
-        let offset = match self.offsets.get(&key) {
-            None => 0,
-            Some(offset) => *offset,
-        };
-        match self.logs.get_mut(&key) {
-            None => {
-                let key = key.clone();
-                let logs = vec![Log(offset, msg)];
-                self.logs.insert(key, logs);
-            }
-            Some(logs) => logs.push(Log(offset, msg)),
-        };
-        self.offsets.insert(key, offset + 1);
-    }
-
-    fn poll(&self) -> HashMap<String, Vec<Log>> {
-        self.logs.clone()
-    }
-
-    fn commit_offsets(&mut self, key: String, offset: usize) {
-        self.committed.insert(key, offset);
-    }
+  fn insert(&mut self, key: String, offset: usize) {
+    self.logs.insert(key, offset);
+  }
 }
 
 #[derive(Clone, Copy, Serialize, Deserialize)]
@@ -104,17 +183,17 @@ struct Log(usize, usize);
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "snake_case", tag = "type")]
 enum Request {
-    Send { key: String, msg: usize },
-    Poll {},
-    Commit {},
-    LimitCommittedOffsets {},
+  Send { key: String, msg: usize },
+  Poll { offsets: HashMap<String, usize> },
+  CommitOffsets { offsets: HashMap<String, usize> },
+  ListCommittedOffsets { keys: Vec<String> },
 }
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "snake_case", tag = "type")]
 enum Response {
-    SendOk {},
-    PollOk { msgs: HashMap<String, Vec<Log>> },
-    CommitOk {},
-    LimitCommitedOffsetsOk {},
+  SendOk { offset: usize },
+  PollOk { msgs: HashMap<String, Vec<Log>> },
+  CommitOffsetsOk {},
+  ListCommittedOffsetsOk { offsets: HashMap<String, usize> },
 }
